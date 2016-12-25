311a57156711f37745c1bad2df4f9f923216410e
baron@thedevelopmentstore.com <baron@thedevelopmentstore.com>
2016-12-25 21:13:29 +0200

    furst

---
+++ src/IpStorage/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace IpStorage.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var remoteIpAddress = Request.HttpContext.Connection.RemoteIpAddress;
+            var isLocal = remoteIpAddress.ToString() == "::1";
+            var path = Request.Path.ToString();
+            var userAgent = Request.HttpContext.GetHeaderValueAs<string>("User-Agent");
+            var browserName = Utils.GetBrowserName(userAgent);
+
+            IHttpConnectionFeature webConnection = HttpContext.Features.Get<IHttpConnectionFeature>();
+
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ src/IpStorage/Controllers/WebExtentions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+
+namespace IpStorage.Controllers
+{
+    public static class WebExtentions
+    {
+        public static T GetHeaderValueAs<T>(this HttpContext httpContext, string headerName)
+        {
+            StringValues values;
+
+            if (httpContext?.Request?.Headers?.TryGetValue(headerName, out values) ?? false)
+            {
+                string rawValues = values.ToString();   // writes out as Csv when there are multiple.
+
+                if (!string.IsNullOrEmpty(rawValues))
+                {
+                    return (T)Convert.ChangeType(values.ToString(), typeof(T));
+                }
+            }
+            return default(T);
+        }
+
+        public static List<string> SplitCsv(this string csvList, bool nullOrWhitespaceInputReturnsNull = false)
+        {
+            if (string.IsNullOrWhiteSpace(csvList))
+                return nullOrWhitespaceInputReturnsNull ? null : new List<string>();
+
+            return csvList
+                .TrimEnd(',')
+                .Split(',')
+                .AsEnumerable<string>()
+                .Select(s => s.Trim())
+                .ToList();
+        }
+    }
+}
